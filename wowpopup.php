da208b0fab798c6f34a2ba0f2310bde050eb52be
wowthemesnet <wowthemesnet@gmail.com>
2019-07-11 03:19:03 +0300

    1.0.1

--- wowpopup.php
+++ wowpopup.php
@@ -2,59 +2,27 @@
 
 /**
  * @link              https://www.wowthemes.net
- * @since             1.0.0
+ * @since             1.0.1
  * @package           Wowpopup
  *
  * @wordpress-plugin
  * Plugin Name:       Wow Popup
  * Plugin URI:        https://www.wowthemes.net/wow_plugins/
  * Description:       Flexible, yet extremely easy to set up WordPress plugin for PopUps, FlyIns and Notice Bars. Boost your sales in no time, convert your visitors to members, watchers, subscribers, you name it! 
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            WowThemesNet
  * Author URI:        https://www.wowthemes.net
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       wowpopup
  * Domain Path:       /languages
- * @fs_premium_only /public/img/
  */
 if ( !defined( 'ABSPATH' ) ) {
     die;
 }
-// if direct access
-// Create a helper function for easy SDK access.
-function wow_fs()
-{
-    global  $wow_fs ;
-    
-    if ( !isset( $wow_fs ) ) {
-        // Include Freemius SDK.
-        require_once dirname( __FILE__ ) . '/freemius/start.php';
-        $wow_fs = fs_dynamic_init( array(
-            'id'             => '732',
-            'slug'           => 'wowpopup',
-            'type'           => 'plugin',
-            'public_key'     => 'pk_916e5afd8b2fb43cb3a09547f3f1e',
-            'is_premium'     => false,
-            'has_addons'     => false,
-            'has_paid_plans' => true,
-            'trial'          => array(
-            'days'               => 7,
-            'is_require_payment' => false,
-        ),
-            'menu'           => array(
-            'slug'    => 'edit.php?post_type=wow_popup_type',
-            'support' => false,
-        ),
-            'is_live'        => true,
-        ) );
-    }
-    
-    return $wow_fs;
-}
 
-// Init Freemius.
-wow_fs();
+
+
 class WowPopupPlugin
 {
     private  $plugin_name ;
@@ -65,7 +33,7 @@ class WowPopupPlugin
     {
         // Set up default vars
         $this->plugin_name = 'wowpopup';
-        $this->version = '1.0.0';
+        $this->version = '1.0.1';
         $this->plugin_path = plugin_dir_path( __FILE__ );
         $this->plugin_url = plugin_dir_url( __FILE__ );
         // Set up activation hooks
@@ -76,6 +44,7 @@ class WowPopupPlugin
         // Add your own hooks/filters
         add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ) );
         add_action( 'init', array( &$this, 'init' ) );
+        
     }
     
     //  Load Public CSS
@@ -93,6 +62,7 @@ class WowPopupPlugin
     
     public function activate( $network_wide )
     {
+      
     }
     
     public function deactivate( $network_wide )
@@ -106,6 +76,34 @@ class WowPopupPlugin
         }
     }
 
+    
+
 }
 require plugin_dir_path( __FILE__ ) . 'includes/wowpopup-posttype.php';
 new WowPopupPlugin();
+
+
+include plugin_dir_path( __FILE__ ) . 'includes/welcome.php';
+
+register_activation_hook( __FILE__, 'wowpopup_welcome_screen_activate' );
+function wowpopup_welcome_screen_activate() {
+  set_transient( '_welcome_screen_activation_redirect', true, 30 );
+}
+
+add_action( 'admin_init', 'wowpopup_welcome_screen_do_activation_redirect' );
+function wowpopup_welcome_screen_do_activation_redirect() {
+
+    if ( ! get_transient( '_welcome_screen_activation_redirect' ) ) {
+        return;
+    }
+
+    delete_transient( '_welcome_screen_activation_redirect' );
+
+    if ( is_network_admin() || isset( $_GET['activate-multi'] ) ) {
+        return;
+    }
+
+    wp_safe_redirect( add_query_arg( array( 'page' => 'wowpopup_options' ), admin_url( 'edit.php?post_type=wow_popup_type&' ) ) );
+}
+
+
